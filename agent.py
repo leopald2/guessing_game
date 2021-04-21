44afb667be0d128a86cb9d312b3bb1b20c39e896
leopald2 <leopald2@googlemail.com>
2021-04-21 23:15:54 +0200

    Update to Python 3.8

--- agent.py
+++ agent.py
@@ -13,7 +13,7 @@ def find_future_states(state, states_map):
 	return future_states
 
 def find_best_choice(state, future_states):
-	actions_state = future_states.ix[[state]]
+	actions_state = future_states.loc[[state]]
 	if(actions_state.values.all() == 0):
 		best_choice = random.randint(state[0],state[1])
 		# print('random best_choice : ' + str(best_choice))
@@ -33,8 +33,8 @@ def give_choice(best_choice, epsilon, state):
 def update_q_values(states_map, future_states ,state, choice, new_state, alpha, gamma, neg_r):
 	if(new_state != 'won'):
 		future_new_states = find_future_states(new_state, future_states)
-		actions_new_state = future_new_states.ix[[new_state]]
+		actions_new_state = future_new_states.loc[[new_state]]
 		#columns = [column for column in actions_new_state.columns if new_state[0] <= column <= new_state[1]]
 		#max_q_new_state = max(actions_new_state[columns])
 		max_q_new_state = actions_new_state.values.max()
-		states_map.ix[[state], choice] += alpha*(neg_r + gamma*max_q_new_state - states_map.ix[[state], choice])
+		states_map.loc[[state], choice] += alpha*(neg_r + gamma*max_q_new_state - states_map.loc[[state], choice])
